e9bb72d4f678a66032d0d96191d503359c1c11a5
moonwalkeryj <liyuejianglyj@gmail.com>
2014-07-14 19:22:21 +0900

    observers design pattern final

--- observer.py
+++ observer.py
@@ -2,21 +2,42 @@ __author__ = 'Saturn'
 import threading
 import time
 
-class Subject:
-
-    def __init__(self):
+class AbstractSubject:
+    def __init__(self, name):
+        self.name = name
         self.observers = []
         self.status = "default status"
 
+    def attach(self):
+        pass
+    def detach(self):
+        pass
+    def notify(self):
+        pass
+
+
+class Subject(AbstractSubject):
+
+    def __init__(self, name):
+        AbstractSubject.__init__(self,name)
+        # lock to prevent thread access of observers
+        self.lock = threading.Lock()
+
     def attach(self, observer):
+        self.lock.acquire()
         self.observers.append(observer)
+        self.lock.release()
 
     def detach(self, observer):
+        self.lock.acquire()
         self.observers.remove(observer)
+        self.lock.release()
 
     def notify(self):
-        for index in range(len(self.observers)):
-            self.observers[index].update()
+        self.lock.acquire()
+        for observer in self.observers:
+            observer.update()
+        self.lock.release()
 
     def setStatus(self, s):
         self.status = s
@@ -25,16 +46,23 @@ class Subject:
     def getStatus(self):
         return self.status
 
-
-class Observer:
-
+class ObserverBase:
     GlobalID = 0
 
     def __init__(self, subject):
+        print "observer base constructor"
         subject.attach(self)
         self.s = subject
         self.id = Observer.GlobalID
         Observer.GlobalID += 1
+
+    def update(self):
+        pass
+
+class Observer(ObserverBase):
+    def __init__(self, subject):
+        ObserverBase.__init__(self, subject)
+        print "observer inherent constructor"
 
     def update(self):
         print "observer ", self.id, " updated; Subject status: ", self.s.getStatus()
@@ -49,23 +77,28 @@ def runSubjectChangeLong(subject):
         subject.setStatus("status changed: " + str(i+20))
         time.sleep(3)
 
+def deleteObserver(subject, observer, seconds = 4):
+    time.sleep(seconds)
+    subject.detach(observer)
+    print "observer deleted"
+
 if  __name__ == "__main__":
-    subject = Subject()
+    subject = Subject("FirstSubject")
     a = Observer(subject)
     b = Observer(subject)
     c = Observer(subject)
 
     thread1 = threading.Thread(target=runSubjectChange, args=(subject,))
-    thread2 = threading.Thread(target=runSubjectChange, args=(subject,))
-    thread3 = threading.Thread(target=runSubjectChange, args=(subject,))
-    thread4 = threading.Thread(target=runSubjectChangeLong, args=(subject,))
-
+    thread2 = threading.Thread(target=runSubjectChangeLong, args=(subject,))
+    thread3 = threading.Thread(target=deleteObserver, args=(subject, a, 4))
+    thread4 = threading.Thread(target=deleteObserver, args=(subject, b, 5))
+    #
     thread1.start()
     thread2.start()
     thread3.start()
     thread4.start()
 
-    thread1.join()
-    thread2.join()
-    thread3.join()
-    thread4.join()
+    # thread1.join()
+    # thread2.join()
+    # thread3.join()
+    # thread4.join()
